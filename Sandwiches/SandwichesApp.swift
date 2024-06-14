0b04a12ab17f77e94e393541826a92353e9b79ff
Xinwei Fan <luluvision.ivy@gmail.com>
2024-06-14 15:56:48 -0700

    Initial Commit

---
+++ Sandwiches/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Sandwiches
+//
+//  Created by Ivy Fan on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
